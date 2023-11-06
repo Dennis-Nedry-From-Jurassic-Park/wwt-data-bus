8d12fc558034f84ad57ce541778606a6fed93247
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-11-06 11:28:06 +0300

    rm CoinCatEx/nestjs-rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -5,31 +5,10 @@ import {Logger} from "@nestjs/common";
 import {FastifyAdapter, NestFastifyApplication} from "@nestjs/platform-fastify";
 
 async function bootstrap() {
-    // https://github.com/CoinCatEx/nestjs-rabbitmq
-    // const amqp = await NestFactory.create(
-    //     RabbitModule.forRoot({
-    //         host: process.env.AMQP_QUEUE_HOST,
-    //         port: parseInt(process.env.AMQP_QUEUE_PORT, 3),
-    //         login: process.env.AMQP_QUEUE_LOGIN,
-    //         password: process.env.AMQP_QUEUE_PASSWORD,
-    //         tasksQueueNormal: process.env.AMQP_QUEUE_COMMAND_REQUEST,
-    //         tasksQueueRedelivery: process.env.AMQP_QUEUE_REQUEST_ONCE_DELIVERY,
-    //         deadLetterRoutingKey: process.env.AMQP_QUEUE_COMMAND_REQUEST_DEAD_LETTER,
-    //         deadLetterRoutingKeyRedelivery:
-    //         process.env.AMQP_QUEUE_COMMAND_REQUEST_ONCE_DELEVERY_DEAD_LETTER,
-    //         exchange: process.env.AMQP_EXCHANGE_COMMAND,
-    //         prefetch: parseInt(process.env.AMQP_QUEUE_PREFETCH, 3),
-    //     }),
-    // );
-    //const amqpTransport = amqp.get<RabbitTransport>(RabbitTransport);
-    // https://docs.nestjs.com/techniques/performance
+    // TODO: https://github.com/CoinCatEx/nestjs-rabbitmq
     const options = { bodyParser: false }
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), options);
     app.use(json({ limit: '16mb' }));
-    // app.connectMicroservice({
-    //     strategy: amqpTransport,
-    //     options: {},
-    // });
     await app.startAllMicroservices();
     Logger.error('init nestjs')
 
