9de1cdc3ec842dd94edf2483ee5f154170c4920b
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-11-07 11:51:32 +0300

    debug

--- src/exec.ts
+++ src/exec.ts
@@ -3,3 +3,5 @@ console.log(111);
 const obj = JSON.parse("{\"msg\": \"21111111000111111111111A111\"}".toString())
 console.log(obj);
 console.log(typeof obj);
+
+console.log(new Date().toISOString());
