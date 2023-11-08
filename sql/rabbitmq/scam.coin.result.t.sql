f45441f1790f24834d858e8f34b9e336d4f254aa
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-11-08 18:11:35 +0300

    json flatten to flat json

---
+++ sql/rabbitmq/scam.coin.result.t.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE IF NOT EXISTS rmq;
+
+CREATE OR REPLACE TABLE rmq.json_event (
+    timestamp DateTime64(3),
+    id UInt32,
+    routingKey String,
+    body String,
+    _exchange_name String,
+    _channel_id String,
+    _delivery_tag UInt64,
+    _timestamp UInt64
+) Engine = Memory;
+
+CREATE OR REPLACE TABLE rmq.json_event_ (
+        timestamp DateTime64(3),
+        id UInt32,
+        routingKey String,
+        body String,
+        _exchange_name String,
+        _channel_id String,
+        _delivery_tag UInt64,
+        _timestamp UInt64
+) ENGINE = RabbitMQ
+SETTINGS
+    rabbitmq_host_port = '172.17.0.2:5672',
+    rabbitmq_exchange_name = 'exch',
+    rabbitmq_exchange_type = 'direct',
+    rabbitmq_format = 'JSONEachRow',
+    rabbitmq_routing_key_list = 'ms.scam-coin.IsHoneypotCoin',
+    rabbitmq_num_consumers = 1,
+    date_time_input_format = 'best_effort';
+    //rabbitmq_address = 'amqp://zowie:2840@localhost:5672',
+
+DROP VIEW rmq.event_view
+
+CREATE MATERIALIZED VIEW rmq.json_event_view
+TO rmq.event AS
+SELECT
+    now() as timestamp,
+    id AS id,
+    routingKey AS routingKey,
+    body AS body,
+    _exchange_name AS _exchange_name,
+    _channel_id AS _channel_id,
+    _delivery_tag AS _delivery_tag,
+    _timestamp AS _timestamp
+FROM rmq._;
+
+
+
+
+
+
+
+
+
--- src/exec.ts
+++ src/exec.ts
@@ -86,9 +86,92 @@ console.log(pairAddress);
 // o['liquidity'] = pairs.Liquidity
 // console.log(o);
 
+//import { flatten } from 'flat'
+//import { flatten } from 'flat'
 
+function flattenObject(obj, separator = '.') {
+    let result = {};
+    function recurse(cur, prop) {
+        if (Object(cur) !== cur) {
+            result[prop] = cur;
+        } else if (Array.isArray(cur)) {
+            for (let i = 0, l = cur.length; i < l; i++) {
+                recurse(cur[i], prop + separator + i);
+                if (l == 0) result[prop] = [];
+            }
+
+
+        } else {
+            let isEmpty = true;
+            for (let p in cur) {
+                isEmpty = false;
+                recurse(cur[p], prop ? prop + separator + p : p);
+            }
+            if (isEmpty && prop) result[prop] = {};
+        }
+    }
+    recurse(obj, '');
+    return result;
+}
 
 const algo = async () => {
+    const obj = {
+        'token.name': 'NITRO',
+        'token.symbol': 'NITRO',
+        'token.decimals': 18,
+        'token.address': '0x95B6cD4aCb05A4e3B682f009FD323BB1D822F5cF',
+        'token.totalHolders': 9,
+        'withToken.name': 'Wrapped Ether',
+        'withToken.symbol': 'WETH',
+        'withToken.decimals': 18,
+        'withToken.address': '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+        'withToken.totalHolders': 740364,
+        'simulationSuccess': true,
+        'honeypotResult.isHoneypot': true,
+        'honeypotResult.honeypotReason': 'execution reverted: UniswapV2: INSUFFICIENT_OUTPUT_AMOUNT',
+        'simulationResult.buyTax': 0,
+        'simulationResult.sellTax': 100,
+        'simulationResult.transferTax': 0,
+        'simulationResult.buyGas': '0',
+        'simulationResult.sellGas': '0',
+        'holderAnalysis.holders': '6',
+        'holderAnalysis.successful': '6',
+        'holderAnalysis.failed': '0',
+        'holderAnalysis.siphoned': '0',
+        'holderAnalysis.averageTax': 97.87166666666667,
+        'holderAnalysis.averageGas': 275828,
+        'holderAnalysis.highestTax': 97.97,
+        'holderAnalysis.highTaxWallets': '6',
+        'holderAnalysis.taxDistribution.0.tax': 97,
+        'holderAnalysis.taxDistribution.0.count': 6,
+        'flags.0': 'EXTREMELY_HIGH_TAXES',
+        'flags.1': 'high_tax',
+        'chain.id': '1',
+        'chain.name': 'Ethereum',
+        'chain.shortName': 'eth',
+        'chain.currency': 'ETH',
+        'router': '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+        'pair.pair.name': 'Uniswap V2: NITRO-WETH',
+        'pair.pair.address': '0x3785FCDd03Ab373Bf75E787C12ABeC72Fb8e2741',
+        'pair.pair.token0': '0x95B6cD4aCb05A4e3B682f009FD323BB1D822F5cF',
+        'pair.pair.token1': '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+        'pair.pair.type': 'UniswapV2',
+        'pair.chainId': '1',
+        'pair.reserves0': '1298907',
+        'pair.reserves1': '1',
+        'pair.liquidity': 0,
+        'pair.router': '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+        'pair.createdAtTimestamp': '1698164795',
+        'pair.creationTxHash': '0xca0f690b9c014f12d3fd840ef6d68ff53ef743c4262364f025a8652b5e26f872',
+        'pairAddress': '0x3785FCDd03Ab373Bf75E787C12ABeC72Fb8e2741'
+    }
+    const obj_ = JSON.stringify(obj)
+console.log(obj_);
+}
+const algo1 = async () => {
+    console.log(flattenObject(msg));
+}
+const algo2 = async () => {
     if(msg.honeypotResult.isHoneypot) {
         console.log(msg.honeypotResult.isHoneypot);
     }
