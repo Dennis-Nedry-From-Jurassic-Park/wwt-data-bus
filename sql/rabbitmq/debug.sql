7565ead8704eb8fd8527d91290994e4614795957
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-11-10 15:11:37 +0300

    clickhouse: table size scripts

--- sql/rabbitmq/debug.sql
+++ sql/rabbitmq/debug.sql
@@ -13,7 +13,10 @@ GROUP BY
     table
 ORDER BY size DESC;
 
-/* размер compressed и uncompressed для каждой колонки */
+/*
+    размер compressed и uncompressed для каждой колонки
+    https://chistadata.com/knowledge-base/how-to-check-table-and-column-sizes-in-clickhouse/
+*/
 SELECT
     database,
     table,
@@ -24,9 +27,41 @@ SELECT
     sum(rows) rows_cnt,
     round(usize / rows_cnt, 2) avg_row_size
 FROM system.parts_columns
-WHERE (active = 1) AND (database LIKE 'rmq') AND (table LIKE '%')
+WHERE (active = 1) AND (database LIKE 'rmq') AND (table LIKE 'flatten_json0')
 GROUP BY
     database,
     table,
     column
 ORDER BY size DESC;
+
+
+SELECT
+    name,
+    formatReadableSize(sum(data_compressed_bytes)) AS compressed_size,
+    formatReadableSize(sum(data_uncompressed_bytes)) AS uncompressed_size,
+    round(sum(data_uncompressed_bytes) / sum(data_compressed_bytes), 2) AS ratio
+FROM system.columns
+WHERE table = 'flatten_json0'
+GROUP BY name
+ORDER BY sum(data_compressed_bytes) DESC
+
+SELECT
+    formatReadableSize(sum(data_compressed_bytes)) AS compressed_size,
+    formatReadableSize(sum(data_uncompressed_bytes)) AS uncompressed_size,
+    round(sum(data_uncompressed_bytes) / sum(data_compressed_bytes), 2) AS ratio
+FROM system.columns
+WHERE table = 'flatten_json0'
+
+
+
+select concat(database, '.', table)                         as table,
+       formatReadableSize(sum(bytes))                       as size,
+       sum(rows)                                            as rows,
+       max(modification_time)                               as latest_modification,
+       sum(bytes)                                           as bytes_size,
+       any(engine)                                          as engine,
+       formatReadableSize(sum(primary_key_bytes_in_memory)) as primary_keys_size
+from system.parts
+where active
+group by database, table
+order by bytes_size desc;
