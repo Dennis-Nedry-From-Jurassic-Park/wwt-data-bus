75d10d65fd7fb1e81872ed0da5059d4f8e01530e
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-11-10 15:11:49 +0300

    flatten json sql

--- sql/rabbitmq/scam.coin.flatten.json.t.sql
+++ sql/rabbitmq/scam.coin.flatten.json.t.sql
@@ -94,7 +94,10 @@ SELECT
     _timestamp AS _timestamp
 FROM rmq.flatten_json_rabbitmq;
 
-
+multiple! -> table size
+INSERT INTO rmq.flatten_json (rawJSON) VALUES (
+'{"token.name":"NITRO","token.symbol":"NITRO","token.decimals":18,"token.address":"0x95B6cD4aCb05A4e3B682f009FD323BB1D822F5cF","token.totalHolders":9,"withToken.name":"Wrapped Ether","withToken.symbol":"WETH","withToken.decimals":18,"withToken.address":"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2","withToken.totalHolders":740364,"simulationSuccess":true,"honeypotResult.isHoneypot":true,"honeypotResult.honeypotReason":"execution reverted: UniswapV2: INSUFFICIENT_OUTPUT_AMOUNT","simulationResult.buyTax":0,"simulationResult.sellTax":100,"simulationResult.transferTax":0,"simulationResult.buyGas":"0","simulationResult.sellGas":"0","holderAnalysis.holders":"6","holderAnalysis.successful":"6","holderAnalysis.failed":"0","holderAnalysis.siphoned":"0","holderAnalysis.averageTax":97.87166666666667,"holderAnalysis.averageGas":275828,"holderAnalysis.highestTax":97.97,"holderAnalysis.highTaxWallets":"6","holderAnalysis.taxDistribution.0.tax":97,"holderAnalysis.taxDistribution.0.count":6,"flags.0":"EXTREMELY_HIGH_TAXES","flags.1":"high_tax","chain.id":"1","chain.name":"Ethereum","chain.shortName":"eth","chain.currency":"ETH","router":"0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D","pair.pair.name":"Uniswap V2: NITRO-WETH","pair.pair.address":"0x3785FCDd03Ab373Bf75E787C12ABeC72Fb8e2741","pair.pair.token0":"0x95B6cD4aCb05A4e3B682f009FD323BB1D822F5cF","pair.pair.token1":"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2","pair.pair.type":"UniswapV2","pair.chainId":"1","pair.reserves0":"1298907","pair.reserves1":"1","pair.liquidity":0,"pair.router":"0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D","pair.createdAtTimestamp":"1698164795","pair.creationTxHash":"0xca0f690b9c014f12d3fd840ef6d68ff53ef743c4262364f025a8652b5e26f872","pairAddress":"0x3785FCDd03Ab373Bf75E787C12ABeC72Fb8e2741"}'
+);
 
 
 
