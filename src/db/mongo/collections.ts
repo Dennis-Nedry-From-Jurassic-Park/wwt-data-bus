4cc803d2b8cf5a9bac7b9b66800a93fde646cc5b
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2024-04-07 09:27:48 +0300

    mongodb collections

--- src/db/mongo/collections.ts
+++ src/db/mongo/collections.ts
@@ -1,7 +1,10 @@
 export const Collection = {
     data: 'data',
     logs: 'logs',
-    eth_mainnet: 'eth.mainnet'
+    eth_mainnet: 'eth.mainnet',
+    // 4byte
+    signatures: '4byte.signatures',
+    eventsignatures: '4byte.eventsignatures',
 }
 
 export type Collection = typeof Collection[keyof typeof Collection]
