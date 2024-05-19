a7bfe584d54b29605fdfd77e8bc529109664b3de
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2024-05-19 11:21:30 +0300

    clickhouse db: add table ankr_getTokenTransfers

--- db/create.sql
+++ db/create.sql
@@ -8,5 +8,19 @@ CREATE TABLE wwt.ankr_getTransactionsByAddress
 ENGINE = MergeTree
 ORDER BY timestamp
 
+
 --drop table wwt.ankr_getTransactionsByAddress
 --truncate table wwt.ankr_getTransactionsByAddress
+
+
+set allow_experimental_object_type = 1;
+
+CREATE TABLE wwt.ankr_getTokenTransfers
+(
+    timestamp DateTime64(3, 'UTC'),
+    data JSON CODEC(ZSTD(3)),
+    address String CODEC(LZ4),
+    nextPageToken String CODEC(LZ4)
+)
+ENGINE = MergeTree
+ORDER BY timestamp
