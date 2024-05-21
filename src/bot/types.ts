a2f55686954e4d3d8999709d1b977dce2d32949a
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2024-05-21 17:04:16 +0300

    add ankr tables

--- src/bot/types.ts
+++ src/bot/types.ts
@@ -16,6 +16,9 @@ export const Table = {
     wwt_ankr_getTransactionsByAddress_Temp: "wwt.ankr_getTransactionsByAddress_Temp",
     wwt_ankr_getTransactionsByAddress: "wwt.ankr_getTransactionsByAddress",
     wwt_ankr_getTokenTransfers: "wwt.ankr_getTokenTransfers",
+    wwt_ankr_getAccountBalance: "wwt.ankr_getAccountBalance",
+    wwt_ankr_getAccountBalanceHistorical: "wwt.ankr_getAccountBalanceHistorical",
+    wwt_ankr_temp: "wwt.ankr_temp",
 }
 export type Table = typeof Table[keyof typeof Table]
 
