91538436b3d00c968fff1039637317bd622d43f3
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2024-05-23 14:18:15 +0300

    api for new coins listings

---
+++ src/bot/new-listings/_.ts
@@ -0,0 +1,2 @@
+// https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyListingsNew
+// https://coinmarketcap.com/api/documentation/v1/#operation/getV2CryptocurrencyOhlcvHistorical
