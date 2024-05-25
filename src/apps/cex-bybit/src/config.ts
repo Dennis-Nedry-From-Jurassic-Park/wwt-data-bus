e92568d767f294cdf106a497f2184679c432e17c
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2024-05-25 17:30:56 +0300

    ws bybit config

---
+++ src/apps/cex-bybit/src/config.ts
@@ -0,0 +1,28 @@
+import {WSClientConfigurableOptions} from "bybit-api/lib/types";
+
+const API_KEY = process.env.CEX_BYBIT_API_V5_PUBKEY;
+const PRIVATE_KEY = process.env.CEX_BYBIT_API_V5_PRIVATEKEY;
+
+export const ws_options: WSClientConfigurableOptions= {
+    market: 'v5',
+    testnet: false,
+    key: API_KEY,
+    secret: PRIVATE_KEY,
+    pongTimeout: 30_000,
+    // how often to check (in ms) that WS connection is still alive
+    pingInterval: 10000,
+    // config options sent to RestClient (used for time sync). See RestClient docs.
+    // restOptions: { },
+
+    // config for axios used for HTTP requests. E.g for proxy support
+    // requestOptions: { }
+
+    // how long to wait before attempting to reconnect (in ms) after connection is closed
+    // reconnectTimeout: 500,
+
+    // recv window size for authenticated websocket requests (higher latency connections (VPN) can cause authentication to fail if the recv window is too small)
+    // recvWindow: 5000,
+
+    // override which URL to use for websocket connections
+    // wsUrl: 'wss://stream.bytick.com/realtime'
+}
