c519c6307636bc2c90e08f58a6e5bcc8987f2199
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2024-05-27 10:03:50 +0300

    add clickhouse nestjs

--- src/apps/cex-bybit/src/app.module.ts
+++ src/apps/cex-bybit/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {BybitWebSocketGateway} from "./websocket-gateway";
+import {ClickHouseModule} from "@depyronick/nestjs-clickhouse";
+import {defaultOptions} from "../../../db/clickhouse/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ClickHouseModule.register(defaultOptions)
+  ],
   controllers: [AppController],
   providers: [AppService, BybitWebSocketGateway],
 })
