e10111a0e024f477b25c3574a95ae5cc54c9135d
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2024-05-27 10:06:10 +0300

    Fastify bufferLogs

--- src/apps/cex-bybit/src/main.ts
+++ src/apps/cex-bybit/src/main.ts
@@ -5,9 +5,10 @@ import {FastifyAdapter} from "@nestjs/platform-fastify";
 async function bootstrap() {
   const app = await NestFactory.create(
       AppModule,
-      new FastifyAdapter({ logger: true })
+      new FastifyAdapter({ logger: true }),
+      { bufferLogs: true }
   );
   await app.listen(3000);
 }
 bootstrap();
-// TODO: https://dropstab.com/p/cool-head-7xjurwmfvt
+// TODO: https://www.dropstab.com/p/cool-head-7xjurwmfvt
