636813df787705f5a4ab53fc849b9ce99d93a8e4
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2024-05-27 09:59:37 +0300

    try generate secret signature X-BAPI hmac bybit

--- src/apps/cex-bybit/src/app.controller.ts
+++ src/apps/cex-bybit/src/app.controller.ts
@@ -1,12 +1,59 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get, Inject, Post, Req} from '@nestjs/common';
+import {AppService} from './app.service';
+import {token} from "../../../db/clickhouse/config";
+import {ClickHouseClient} from "@depyronick/nestjs-clickhouse";
+import {BybitTemp} from "./dto";
 
-@Controller()
+@Controller('bybit')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        @Inject(token)
+        private clickHouseClient: ClickHouseClient
+    ) {
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    }
+
+    // TODO: getOpenInterest getHistoricOrders
+
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Post('getWalletBalance')
+    async getWalletBalance(@Req() req): Promise<any> {
+        console.log(req.body);
+        return await this.appService.client.getWalletBalance(req.body);
+    } // TODO https://bybit-exchange.github.io/docs/v5/account/wallet-balance
+
+    // https://github.com/tiagosiebler/bybit-api/blob/master/examples/rest-v5-private.ts
+    @Post('submitOrder')
+    async order(@Req() req): Promise<any> {
+        const body = req.body
+        console.log({body});
+        const orderResult = await (await this.appService.generateClient()).submitOrder({
+            category: body.category,
+            symbol: body.symbol,
+            orderType: body.orderType,
+            qty: body.qty,
+            side: body.side,
+        });
+        this.clickHouseClient.insert<BybitTemp>(
+            "wwt.bybit_temp", [
+                {
+                    timestamp: Date.now(),
+                    router: `submitOrder.${body.category}.${body.orderType}.${body.side}.${body.qty}`,
+                    data: orderResult,
+                    version: 5
+                }
+            ]
+        ).subscribe({
+            error: (err: any): void => {
+                console.error({err});
+            },
+        });
+        console.log({orderResult});
+        return orderResult;
+    }
 }
